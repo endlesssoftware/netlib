87df11293e6a84fafc73802e62d268cdad15bd0d
Tim E. Sneddon <tim@sneddon.id.au>
2013-02-08 10:07:03 +0800

    Modified to include help macros and I/O specific fields.

--- netlib_ssl.h
+++ netlib_ssl.h
@@ -7,6 +7,14 @@
 #include <openssl/ssl.h>
 #include "netlibdef.h"
 
+/*
+** Socket-level SSL specific fields.
+*/
+#define spec_socket	specctx->socket
+#define spec_ssl	specctx->ssl
+#define spec_rbio	specctx->rbio
+#define spec_wbio	specctx->wbio
+
     struct SPECCTX {
 	void *socket;
 	SSL *ssl;
@@ -14,6 +22,12 @@
     };
 #define __SPECCTX struct SPECCTX
 #define SPECCTX_SIZE sizeof(__SPECCTX)
+
+/*
+** I/O-level SSL specific fields.
+*/
+#define spec_call	arg[0].address
+#define spec_argv	arg[1].address
 
     struct SPECIOR {
     	unsigned short fromlen;
